50dece35477644a67d33539e6a13dd0a157fd94c
salina kuo <salina.kuo97@gmail.com>
2020-11-10 17:46:08 -0500

    pet detail list

---
+++ src/PetDetails.js
@@ -0,0 +1,9 @@
+import { getDefaultNormalizer } from "@testing-library/react"
+import React from "react"
+import PetDetailList from './PetDetailList.js';
+
+const PetDetails = props => (
+    <PetDetailList pet={props.pet} />
+)
+
+export default PetDetails;
