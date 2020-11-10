98a2ce41f11a571eac6f94b7d1a7cb9176c790cd
julia-richards <julia.m.richards@gmail.com>
2020-11-10 14:51:54 -0700

    rendered navigation from pet detail page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React from 'react';
+import React from "react";
+import PetDetailPage from "./PetDetailPage";
 
 function App() {
-  return (
-    <h1>Hello</h1>
-  );
+	return <PetDetailPage></PetDetailPage>;
 }
 
 export default App;
